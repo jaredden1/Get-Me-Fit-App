7364bc3d670792aada2bd92607b43c9323cf70b2
dustinowen <137654369+dustinowen@users.noreply.github.com>
2023-08-14 08:45:56 -0700

    Add delete function (working). Update date display format.

--- routes/workouts.js
+++ routes/workouts.js
@@ -7,5 +7,6 @@ router.get("/", workoutCtrl.index);
 router.get("/new", workoutCtrl.new);
 router.get("/:id", workoutCtrl.show);
 router.post("/", workoutCtrl.create);
+router.delete("/:id", workoutCtrl.delete)
 
 module.exports = router;
